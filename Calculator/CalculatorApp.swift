0dbb8665f966ef924d68a0a3b0f123e53947b4c2
amit1020 <amitlevi3403@gmail.com>
2022-05-13 12:37:32 +0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Amit Levi on 13/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
